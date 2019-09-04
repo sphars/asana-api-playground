52c8a8e95fc1583afd6b3e1a0d91c380afba2be9
Spencer Harston <spencer314@gmail.com>
2019-09-04 14:07:58 -0600

    Create a menu system

---
+++ AsanaAPIPlayground/Menu.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsanaAPIPlayground
+{
+    public class MenuItem
+    {
+        public string Description { get; set; }
+        public bool HasSubmenu { get; set; }
+        public int? SubmenuID { get; set; }
+        public Action Execute { get; set; }
+    }
+
+    public class Menu
+    {
+        public List<MenuItem> MenuItems;
+        public int ID { get; set; }
+        public string Title { get; set; }
+
+        public Menu()
+        {
+            MenuItems = new List<MenuItem>();
+        }
+
+        public void PrintMenu()
+        {
+            Console.WriteLine();
+            Console.WriteLine("---{0} Menu---", Title);
+            foreach (MenuItem menuItem in MenuItems)
+            {
+                Console.WriteLine(" {0} - {1}", MenuItems.IndexOf(menuItem), menuItem.Description);
+            }
+        }
+
+    }
+
+    public class MenuCollection
+    {
+        public List<Menu> Menus { get; set; }
+
+        public MenuCollection()
+        {
+            Menus = new List<Menu>();
+        }
+
+        public void ShowMenu(int id)
+        {
+            //get the menu we want to display and print it
+            var currentMenu = Menus.Where(m => m.ID == id).Single();
+            currentMenu.PrintMenu();
+
+            //get user input
+            Console.WriteLine();
+            Console.Write("Enter your choice: ");
+            string choice = Console.ReadLine();
+
+            //validate user input
+            if (!int.TryParse(choice, out int choiceIndex) || currentMenu.MenuItems.Count <= choiceIndex || choiceIndex < 0)
+            {
+                Console.Clear();
+                Console.WriteLine("Invalid selection.");
+                ShowMenu(id);
+            }
+            else
+            {
+                //retrieve the selected choice
+                var selectedMenuItem = currentMenu.MenuItems[choiceIndex];
+
+                //check if there's a submenu
+                if (selectedMenuItem.HasSubmenu)
+                {
+                    Console.Clear();
+                    ShowMenu(selectedMenuItem.SubmenuID.Value);
+                }
+                else
+                {
+                    //execute the menu's action
+                    selectedMenuItem.Execute();
+
+                    //return to previous menu
+                    ShowMenu(id); 
+                }
+            }
+        }
+    }
+
+}
--- AsanaAPIPlayground/Program.cs
+++ AsanaAPIPlayground/Program.cs
@@ -8,8 +8,125 @@ namespace AsanaAPIPlayground
 {
     class Program
     {
+        private static MenuCollection menuCollection;
+        private static bool runApp = true;
+
         static void Main(string[] args)
         {
+            CreateMenus();
+
+            Console.WriteLine("Welcome to the Asana API.");
+            Console.WriteLine("------------------------------");
+
+            //show the main menu
+            menuCollection.ShowMenu(1);
+        }
+
+        static void CreateMenus()
+        {
+            menuCollection = new MenuCollection()
+            {
+                Menus =
+                {
+                    new Menu()
+                    {
+                        ID = 1,
+                        Title = "Main",
+                        MenuItems =
+                        {
+                            new MenuItem()
+                            {
+                                Description = "Show your user info",
+                                HasSubmenu = false,
+                                Execute = GetSelf
+                            },
+                            new MenuItem()
+                            {
+                                Description = "List projects",
+                                HasSubmenu = true,
+                                SubmenuID = 2,
+                                Execute = () =>
+                                {
+
+                                }
+                            },
+                            new MenuItem()
+                            {
+                                Description = "Exit",
+                                HasSubmenu = false,
+                                Execute = ExitApp
+                            }
+                        }
+                    },
+                    new Menu()
+                    {
+                        ID = 2,
+                        Title = "Projects",
+                        MenuItems =
+                        {
+                            new MenuItem()
+                            {
+                                Description = "Project 1",
+                                HasSubmenu = false,
+                                Execute = () =>
+                                {
+                                    Console.WriteLine("I'm in project 1 action");
+                                }
+                            },
+                            new MenuItem()
+                            {
+                                Description = "Project 2",
+                                HasSubmenu = true,
+                                SubmenuID = 3,
+                                Execute = () => { }
+                            },
+                            new MenuItem()
+                            {
+                                Description = "Return to main menu",
+                                HasSubmenu = true,
+                                SubmenuID = 1,
+                                Execute = () => { }
+                            }
+                        }
+                    },
+                    new Menu()
+                    {
+                        ID = 3,
+                        Title = "Project 2",
+                        MenuItems =
+                        {
+                            new MenuItem()
+                            {
+                                Description = "Display info",
+                                HasSubmenu = false,
+                                Execute = () =>
+                                {
+                                    Console.WriteLine("Project 2 information");
+                                }
+                            },
+                            new MenuItem()
+                            {
+                                Description = "Return",
+                                HasSubmenu = true,
+                                SubmenuID = 2,
+                                Execute = () => { }
+                            }
+                        }
+                    }
+                }
+            };
+        }
+
+        public static void ExitApp()
+        {
+            Console.WriteLine("Exiting...");
+            runApp = false;
+            Environment.Exit(0);
+        }
+
+        public static void GetSelf()
+        {
+            Console.WriteLine("Hi there you");
         }
     }
 }
